7def446359eecdf8d43896d10adb2ba5430d32f1
EUYUIL <euyuil@gmail.com>
2012-06-21 14:38:50 +0800

    Added interface for recognization.

---
+++ include/Recognize.h
@@ -0,0 +1,38 @@
+#ifndef _RECOGNIZE_H_
+#define _RECOGNIZE_H_
+
+/**
+ * Interface for digit recognizing algorithms.
+ * @author EUYUIL
+ * @date 2012-06-21
+ */
+
+
+/**
+ * Learn the pattern.
+ * @param r The result that the pattern should lead to.
+ * @param c The pattern matrix.
+ * @param w The width of the matrix.
+ * @param h The height of the matrix.
+ * @return True if learning was succeessful. False otherwise.
+ * @author EUYUIL
+ * @date 2012-06-21
+ */
+
+bool LearnPattern(char r, const char *c, unsigned int w, unsigned int h);
+
+
+/**
+ * Recognize the pattern.
+ * @param c The pattern matrix.
+ * @param w The width of the matrix.
+ * @param h The height of the matrix.
+ * @return The recognized character of the pattern.
+ * @author EUYUIL
+ * @date 2012-06-21
+ */
+
+char Recognize(const char *c, unsigned int w, unsigned int h);
+
+
+#endif /* _RECOGNIZE_H_ */
---
+++ src/Recognize.cpp
@@ -0,0 +1,13 @@
+#include <Recognize.h>
+
+bool LearnPattern(char r, const char *c, unsigned int w, unsigned int h)
+{
+    // TODO
+    return false;
+}
+
+char Recognize(const char *c, unsigned int w, unsigned int h)
+{
+    // TODO
+    return ' ';
+}
